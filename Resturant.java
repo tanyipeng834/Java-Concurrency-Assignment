fc3ad9ac04b9ee8d3a7c9a6976ca7c49c38ba8ed
Tan Yi Peng <61421451+tanyipeng834@users.noreply.github.com>
2025-12-26 16:06:08 +0800

    feat: finished file processing for resturant.java

--- Resturant.java
+++ Resturant.java
@@ -1,6 +1,111 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Resturant{
-public static void main(String[] args) {
-System.out.println("hello World");    
+
+    static final class Config{
+        final int Chefs;
+        final int Waiters;
+        final int Orders;
+        final int placementOrderTime;
+        final int preparationOrderTime;
+        final int serveOrderTime;
+        final int orderQueueSize;
+        final int serveQueueSize;
+
+        Config(int Chefs, int Waiters, int Orders, int placementOrderTime, int preparationOrderTime,int serveOrderTime, int orderQueueSize, int serveQueueSize){
+            this.Chefs = Chefs;
+            this.Waiters=Waiters;
+            this.Orders= Orders;
+            
+            this.placementOrderTime = placementOrderTime;
+            this.preparationOrderTime = preparationOrderTime;
+            this.serveOrderTime = serveOrderTime;
+            this.orderQueueSize = orderQueueSize;
+            this.serveQueueSize = serveQueueSize;
+
+        }
+       @Override
+public String toString() {
+    return "Config{" +
+            "Chefs=" + Chefs +
+            ", Waiters=" + Waiters +
+            ", Orders=" + Orders +
+            ", placementOrderTime=" + placementOrderTime +
+            ", preparationOrderTime=" + preparationOrderTime +
+            ", serveOrderTime=" + serveOrderTime +
+            ", orderQueueSize=" + orderQueueSize +
+            ", serveQueueSize=" + serveQueueSize +
+            '}';
+}
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Config inputConfig = readUserInput("config.txt");
+        System.out.println(inputConfig);
+
+  
+
+}
+
+static Config readUserInput(String fileName){
+    // Create an static array for the config object values
+    int[] configValues = new int[8];
+    File userInputFile = new File(fileName);
+    
+    try(Scanner fileReader = new Scanner(userInputFile)){
+        int i =0;
+        while(fileReader.hasNextLine()){
+            String line = fileReader.nextLine();
+            // Remove comment
+            int hash = line.indexOf("#");
+            if (hash>=0){
+                line = line.substring(0,hash);
+            }
+        line = line.trim();
+        if (line.isEmpty()){
+            System.err.println("Error: Empty Line is not allowed ");
+            System.exit(1);
+
+        }
+        int option;
+
+        try{
+            
+            option = Integer.parseInt(line);
+            configValues[i] = option;
+            i++;
+
+            System.out.println(option);
+            if (option<0) throw new NumberFormatException();
+        }
+        catch (NumberFormatException e ){
+            System.err.println("Error: Not a valid Integer");
+            System.exit(1);
+        }
+
+        }
+       
+
+    }
+    catch(FileNotFoundException e ){
+        System.out.println("File is not Config.txt");
+    }
+
+
+    return new Config(configValues[0], configValues[1], configValues[2], configValues[3], configValues[4], configValues[5],configValues[6], configValues[7]);
+
+
+    
+
+
+
 
 }
 }
+
+
