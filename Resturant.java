66366bd57e940143083914cf52ffa1d7b4a5be58
Tan Yi Peng <61421451+tanyipeng834@users.noreply.github.com>
2025-12-28 11:10:25 +0800

    feat: added locks for synchronization

--- Resturant.java
+++ Resturant.java
@@ -4,16 +4,222 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 public class Resturant{
 
     static BlockingQueue<Integer> orderPlacementQueue;
     static BlockingQueue<Integer> preparedOrderQueue;
-    static ThreadPoolExecutor chefPool;
-    static ThreadPoolExecutor waiterPool;
-    static int currentOrderNo;
+    static final int POISON =-1;
+   
+    static volatile int currentOrderNo;
+    static volatile int currentServeNo;
+    static volatile int currentCookNo;
+    // LOCK FOR Shared object currentOrderNo
+    static final Object ORDER_LOCK = new Object();
+    // Lock for Shared object currentServeNo
+    static final Object SERVE_LOCK = new Object();
+    static final Object COOK_LOCK = new Object();
+    static void prepareOrder(int preparationOrderTime, int chefId){
+            try{
+                int orderId = orderPlacementQueue.take();
+                Thread.sleep(preparationOrderTime);
+
+                preparedOrderQueue.put(orderId);
+                 System.out.printf("Chef %d Order Prepared- Order %d\n",chefId,orderId);
+
+                 synchronized(COOK_LOCK){
+                    currentCookNo++;
+                 }
+                
+
+
+            }
+
+            catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+
+        }
+    static void takeOrder(int placementOrderTime, int waiterId){
+        int orderId;
+            try{
+                Thread.sleep(placementOrderTime);
+
+            }
+
+            catch(InterruptedException e){
+                
+                Thread.currentThread().interrupt();
+
+            }
+            try{
+                
+                synchronized(ORDER_LOCK){
+                    
+                   orderId= currentOrderNo ++;
+
+                }
+
+                    orderPlacementQueue.put(orderId);
+                    
+                    System.out.printf("Waiter %d Order Placed - Order %d\n",waiterId,orderId);
+            
+            
+           
+            
+            }
+            catch(InterruptedException e){
+                Thread.currentThread().interrupt();
+
+            }
+        }
+         static void serveOrder(int serveOrderTime, int waiterId){
+            try{
+                int orderId = preparedOrderQueue.take();
+                System.out.printf("Waiter %d Order Served - Order %d\n",waiterId,orderId);
+                
+                // Time taken to serve to the customer
+                Thread.sleep(serveOrderTime);
+                synchronized(SERVE_LOCK){
+
+                currentServeNo ++;
+                }
+
+
+            }
+
+            catch(InterruptedException e){
+                
+                Thread.currentThread().interrupt();
+
+            }
+         
+      
+
+
+        }
+
+
+    static class ChefTask implements Runnable{
+        private final int chefId;
+        private final int preparationOrderTime;
+        private BlockingQueue<Integer> preparedOrderQueue;
+        private final int Orders;
+
+        ChefTask( int chefId, int preparationOrderTime, int Orders)
+        {
+            this.chefId = chefId;
+            this.preparationOrderTime = preparationOrderTime;
+            this.preparedOrderQueue = Resturant.preparedOrderQueue;
+            this.Orders = Orders;
+
+
+        }
+
+       
+        public void run(){
+            // given that there is nothing to prepare, it will just sleep and then wake up when signalled when there is something put in the queue.
+           
+          
+            
+            while (true){
+                synchronized(COOK_LOCK){
+                    if(currentCookNo>=this.Orders)break;
+                }
+
+              
+            Resturant.prepareOrder(preparationOrderTime,this.chefId);
+          
+            
+                
+        }
+        }
+
+
+
+
+    }
+
+    static class WaiterTask implements Runnable{
+        private final int waiterId;
+        private final int placementOrderTime;
+        private final int serveOrderTime;
+        private BlockingQueue<Integer> orderPlacementQueue;
+        private BlockingQueue<Integer> preparedOrderQueue;
+        private final int Orders;
+        
+        WaiterTask(int waiterId,int placementOrderTime,int serveOrderTime,int Orders){
+            this.waiterId = waiterId;
+            this.placementOrderTime = placementOrderTime;
+            this.serveOrderTime = serveOrderTime;
+            this.preparedOrderQueue = Resturant.preparedOrderQueue;
+            this.orderPlacementQueue = Resturant.orderPlacementQueue;
+            this.Orders = Orders;
+
+        }
+
+        // Only one waiter can take the order at any given time
+
+         
+            
+           
+
+
+
+        
+        //
+       
+
+        @Override
+        public void run()
+        {
+            // only terminate when all orders have been served, check condition variable when thread wakes up
+            
+
+
+             while (true){
+
+                while(true){
+                     synchronized (ORDER_LOCK) {
+                            if (currentOrderNo >= Orders) {
+                                break;}
+                        Resturant.takeOrder(placementOrderTime,waiterId); // or inline the increment here
+                    }
+                }
+              
+
+
+               synchronized (SERVE_LOCK) {
+            if (currentServeNo >= this.Orders) break;
+                                                        }
+               
+                Resturant.serveOrder(serveOrderTime,waiterId);
+
+               
+               // break from the loop and exit the thread.
+               
+                                                    }
+                
+
+           
+                   
+                
+
+           
+      
+          
+                    
+                    
+
+            
+           
+            
+           
+
+
+
+        }
+
+    }
 
 
    
@@ -68,6 +274,10 @@ public String toString() {
         orderPlacementQueue = new ArrayBlockingQueue<>(inputConfig.orderQueueSize);
         preparedOrderQueue = new ArrayBlockingQueue<>(inputConfig.serveQueueSize);
 
+        createWaiters(inputConfig.Waiters, inputConfig.placementOrderTime, inputConfig.preparationOrderTime, inputConfig.serveOrderTime, inputConfig.Orders);
+        createChefs(inputConfig.Chefs,inputConfig.preparationOrderTime,inputConfig.Orders);
+
+
 
 
   
@@ -103,7 +313,7 @@ static Config readUserInput(String fileName){
             configValues[i] = option;
             i++;
 
-            System.out.println(option);
+            
             if (option<0) throw new NumberFormatException();
         }
         catch (NumberFormatException e ){
@@ -130,16 +340,25 @@ static Config readUserInput(String fileName){
 
 }
 
-static void createWaiters(int noOfWaiters){
+static void createWaiters(int noOfWaiters, int placementOrderTime, int preparationOrderTime, int serveOrderTime,int orders){
     // Create pool of waiters to execute tasks of order placement and serving
-    waiterPool = new ThreadPoolExecutor(noOfWaiters,noOfWaiters,0L,TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>());
+    for (int i =0;i<noOfWaiters;i++){
+        // Runnable task
+        Thread waiter = new Thread(new WaiterTask(i,placementOrderTime,serveOrderTime,orders));
+        waiter.start();
+    }
+    
     
 
 }
-static void createChefs(int noOfChefs)
+static void createChefs(int noOfChefs,int preparationOrderTime,int Orders)
 {
-    // Create tasks of chef pool which 
-    chefPool = new ThreadPoolExecutor(noOfChefs, noOfChefs, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+    for (int i =0;i<noOfChefs;i++){
+        // Runnable task
+        Thread chef = new Thread(new ChefTask(i, preparationOrderTime, Orders));
+        chef.start();
+    }
+   
 
 }
 
